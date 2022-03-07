c86938cb66b92e2b36d132e7a68b48d50dc516b1
Chris Hancock <69983789+clhancock@users.noreply.github.com>
2022-03-07 10:29:58 -0700

    commit

--- Scripts/R/Correlations.R
+++ Scripts/R/Correlations.R
@@ -7,7 +7,8 @@ tNames  <- names(read.csv(file.path(dataDir,'Data','RegionComposites','T','Media
 time <- as.matrix(read.csv(file.path(dataDir,'Data','RegionComposites','HC','MedianTSbyRegion.csv'))[,'time'])
 regNames <- hcNames[which(hcNames %in% tNames)][-1]
 
-Proxy_HC_T_Rvals = matrix(nrow= 10000,ncol=length(regNames))
+x = 250
+Proxy_HC_T_Rvals = matrix(nrow= x*x,ncol=length(regNames))
 for (reg in regNames){
   HCseries <- read.csv(file.path(dataDir,'Data','RegionComposites','HC',
                                  paste(reg,'.csv',sep='')))
@@ -16,8 +17,8 @@ for (reg in regNames){
   HCseries <- as.matrix(HCseries[which(between(time,0,12000)),])
   T_series <- as.matrix(T_series[which(between(time,0,12000)),])
   time <- as.matrix(time[which(between(time,0,12000)),])
-  data <- corEns(time.1=time, values.1=HCseries[,1:100],
-                 time.2=time, values.2=T_series[,1:100],
+  data <- corEns(time.1=time, values.1=HCseries[,1:x],
+                 time.2=time, values.2=T_series[,1:x],
                  bin.step=100)#,seq(0,12000,100),100,1000)
   Proxy_HC_T_Rvals[,which(regNames==reg)] = as.numeric(data$cor.df$r)
   
