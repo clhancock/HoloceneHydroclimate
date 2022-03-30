95c84f40a3002713346f424c7e0fda9d38a560ad
Chris Hancock <69983789+clhancock@users.noreply.github.com>
2022-03-30 13:23:09 -0700

    commit

--- Scripts/R/conceptual.R
+++ Scripts/R/conceptual.R
@@ -1,7 +1,8 @@
 library(ncdf4) 
 library(ggplot2)
-
-dataDir <-getwd()
+library(ncdf4)
+library(raster)
+#dataDir <-getwd()
 data <- vector(mode='list')
 
 data("wrld_simpl")
@@ -22,11 +23,12 @@ data[['proxyT']] <- apply(proxyT[which(between(proxyT$ages,5500,6500)),],2,mean)
 data[['proxyT']] <- data.frame(lat=seq(-90,60,30)+15,
                                tas=rev(data[['proxyT']][which(grepl('median',names(data[['proxyT']])))[-1]]))
 
+
 #Load Climate Data
-cmip <- nc_open(file.path(dataDir,'Data','Model','cmip6_ANN.nc'))  
+cmip <- nc_open(file.path(dataDir,'Data','Model','cmip6_ANN_regrid.nc'))  
 lats <- ncvar_get(cmip,paste('lat','regrid',sep='_'))
 lons <- ncvar_get(cmip,paste('lon','regrid',sep='_'))
-for (var in c('tas','p-e','pre')){
+for (var in c('p-e')){
   data[[var]] <- vector(mode='list')
   vals <- ncvar_get(cmip,paste(var,'regrid',sep='_'))
   if (var == 'tas'){vals <- vals+273.15}
@@ -44,6 +46,24 @@ for (var in c('tas','p-e','pre')){
                    pctDLand = rep(NA,length(lats)))
   for (y in valL$y){df[which(df$lat == y),'diffLand'] = valL$v[which(valL$y==y)]}
   #df[-1] <- df[-1]-colMeans(df[-1],na.rm=TRUE)[col(df[-1])]
+  for (i in (nrow(df)/2):nrow(df)){
+    if(is.na(df$diffLand[i])){
+      df$diffLand[i] <- df$diff[i]
+    }
+  }
+  running = TRUE
+  if (running){
+    df0 <- df
+    for (i in 1:nrow(df0)){
+      if (abs(df[i,1]) < 40){
+        df[i,2:7] <- apply(df0[max(1,i-5):min(nrow(df0),i+5),2:7],2,mean,na.rm=TRUE)
+      } else if (abs(df0[i,1]) < 80){
+        df[i,2:7] <- apply(df0[max(1,i-3):min(nrow(df0),i+3),2:7],2,mean,na.rm=TRUE)
+      } else{
+        df[i,2:7] <- apply(df0[max(1,i-1):min(nrow(df0),i+1),2:7],2,mean,na.rm=TRUE)
+      }
+    }
+  }
   data[[paste(var,'DF')]] <- df
 }
 
@@ -67,36 +87,72 @@ p[['insolation']] <- base +
   scale_y_continuous(limits=c(-150,300),expand=c(0,0)) 
 
 
-for (var in c('p-e','tas')){
+for (var in c('p-e')){
   if (var == 'tas'){
     pal <- rev(RColorBrewer::brewer.pal(11, 'RdBu')[-c(1,5,7,11)])
     dir <- -1
   } else{
-    pal <- RColorBrewer::brewer.pal(11, 'BrBG')[c(2,2,3,3,4,6,8,9,9,10,10)]
-    dir <- 1
+    pal <- RColorBrewer::brewer.pal(11, 'BrBG')[c(2,3,3,3,4,4,4,5,5,7,7,8,8,8,9,9,9,10)]
+    #pal <- RColorBrewer::brewer.pal(11, 'BrBG')
+    dir <- -1
   } 
+  l <- c(-1,1)*max(abs(data[[paste(var,'DF')]]$diffLand),na.rm=TRUE)
   p[[paste(var,'Diff')]] <- base + 
-    geom_bar(data=data[[paste(var,'DF')]],aes(x=lat,y=1,fill=diff),stat="identity",width=2,alpha=1) +
+    #geom_bar(data=data[[paste(var,'DF')]],aes(x=lat,y=1,fill=diff),stat="identity",width=2,alpha=1) +
     geom_bar(data=data[[paste(var,'DF')]],aes(x=lat,y=1,fill=diffLand),stat="identity",width=2,alpha=1) +
+    #geom_line(data=data[[paste(var,'DF')]],aes(x=lat,y=0.5,color=diffLand),size=100) + 
     scale_fill_gradientn(colors=(pal),
-                         limits=c(-1,1)*max(abs(data[[paste(var,'DF')]]$diffLand),na.rm=TRUE),
-                         na.value=NA)
-  #p[[paste(var,'0ka')]] <- base + 
-  #  geom_bar(data=data[[paste(var,'DF')]],aes(x=lat,y=1,fill=ka0),stat="identity",width=2,alpha=1) +
-  #  scale_fill_distiller(palette=pal,direction=dir,na.value='grey90')
- # p[[paste(var,'6ka')]] <- base + 
-  #  geom_bar(data=data[[paste(var,'DF')]],aes(x=lat,y=1,fill=ka6),stat="identity",width=2,alpha=1) +
-  #  scale_fill_distiller(palette=pal,direction=dir,na.value='grey90')    
+                         limits=l,#breaks=l,
+                         na.value=NA) + 
+    #scale_fill_gradient2(low = pal[2], 
+     #                      mid = 'White', 
+      #                     high = pal[10])+
+    scale_x_continuous(name="Latitude" ,position = "top",
+                        expand=c(0,0), limits= c(-90,90),
+                        breaks=seq(-90,90,30)) +
+    theme(axis.ticks.x  = element_line(color = 'Black',size=0.4), 
+        axis.ticks.length.x =unit(5,"pt"),
+        axis.text.x =element_text(family='sans',size=8,color='Black',angle = -90),
+        axis.title.x =element_text(family='sans',size=8,color='Black'), 
+        plot.margin = unit(c(rep(0.1,3),0.4), "in"),
+        text = element_text(family='sans',size=8),
+        legend.position= c(-0.013, 0.52),
+        legend.title=element_blank()) + 
+    guides(fill = guide_colourbar(frame.linewidth=1,frame.colour=c("Black"),ticks.colour = "Black",
+                                ticks=FALSE,label = FALSE,reverse=TRUE,
+                                barheight=6,barwidth=0.5)) 
 }
 
+plt <- ggdraw(p[['p-e Diff']]) + 
+  draw_label("Dry", x = 0.024, y = 0.71,fontfamily='sans',size=8,angle=-90) +
+  draw_label("Wet", x = 0.024, y = 0.12,fontfamily='sans',size=8,angle=-90)
+
+ggsave(plot=plt, width = 6, height = 2, dpi = 600,device='png',
+       filename = paste(file.path(dataDir,'Figures','conceptual_2.png',sep='')))
 
 
-proxy <- TRUE
+proxy <- FALSE
 if (proxy){
   p[[paste('tas','Diff')]] <- p[[paste('tas','Diff')]] + 
     geom_bar(data=data[['proxyT']],aes(x=lat,y=0.5,fill=tas),stat="identity",width=30,alpha=1) + 
     geom_hline(yintercept = 0.5) 
 }
+
+
+theme(panel.background=element_rect(colour='Black',fill=NA),
+      panel.border    =element_rect(colour='Black',color=,fill=NA),
+      panel.grid.major=element_line(colour='light Grey'),
+      axis.title  = element_blank(),
+      axis.ticks  = element_line(color = 'Black',size=0.4), 
+      axis.text.x = element_blank(),
+      axis.text.y =element_text(family='sans',size=8,color='Black'),
+      axis.ticks.length.y=unit(2,"pt"),
+      axis.ticks.length.x=unit(3,"pt"),
+      plot.margin = unit(c(0, 0.05, 0.05, 0.05), "in"),
+      text = element_text(family='sans',size=8),
+      legend.position='none')
+
+
 
 
 valT <- apply(ncvar_get(cmip,paste('tas','regrid',sep='_'))[,,,2],c(1,2),mean)
@@ -151,7 +207,7 @@ map <- ggdraw(ggplot()+theme_void()+
 
 plt <- ggarrange(pp,p[['p-e Diff']],p[['tas Diff']],p[['insolation']],
                  ncol=1,heights=c(6,1,1,2))
-ggsave(plot=plt, width = 6, height = 4, dpi = 400,device='png',
-       filename = paste(file.path(dataDir,'Figures','conceptual.png',sep='')))
+#ggsave(plot=plt, width = 6, height = 4, dpi = 400,device='png',
+  #     filename = paste(file.path(dataDir,'Figures','conceptual.png',sep='')))
 
 
