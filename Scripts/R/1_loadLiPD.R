b76b9aa1717b5cd0fd1824b4aeb1bc0ebc4d0321
Chris Hancock <69983789+clhancock@users.noreply.github.com>
2022-07-26 18:09:13 -0700

    commit

--- Scripts/R/1_loadLiPD.R
+++ Scripts/R/1_loadLiPD.R
@@ -285,6 +285,166 @@ for (var in names(lipdData)){
 
 
 #Other --------------------------------------------------------------------------------
+
+
+
+
+
+for (regNo in as.numeric(refregions@data[["Acronym"]])){
+  reg <- levels(refregions@data[["Acronym"]])[regNo]
+  print(reg)
+  count <- which(as.numeric(refregions@data[["Acronym"]])==regNo)
+  #
+  #Stack Plot
+  #
+  stack.df <- filterTs(lipdTSO,paste('geo_ipccRegion ==',reg))
+  if (length(stack.df) == 0){next}
+  stack.df <- tidyTs(stack.df,age.var = "age") %>% 
+    filter(between(age,0,12000)) %>% #only years from Holocene
+    group_by(paleoData_TSid) %>%     #group by column
+    arrange(geo_latitude)            #North to south
+  stackColors <- c()
+  #Get colors for categories within region
+  
+  
+  for (category in unique(stack.df$CategorySpecific)){
+    stackColors <- c(stackColors,
+                     plotSettings$colors[which(plotSettings$names == category)])
+  }
+  stackPlot <- plotTimeseriesStack(stack.df,
+                                   time.var = "age",
+                                   color.var =  "CategorySpecific",
+                                   invert.var = 'climateInterpretation1_interpDirection',
+                                   color.ramp = stackColors) + 
+    scale_x_reverse(name = "Age (yr BP)", limits=c(12000,0),expand=c(0,0),
+                    n.breaks=7,sec.axis = sec_axis(~.,labels=NULL)) 
+  #
+  #Region map
+  #
+  proxyDataPrj <- SpatialPointsDataFrame(stack.df[,c("geo_longitude", "geo_latitude")], 
+                                         stack.df, proj4string=CRS(PROJorig))
+  proxyDataPrj <- spTransform(proxyDataPrj, CRSobj = PROJ)
+  proxyDFprj   <- as.data.frame(proxyDataPrj)
+  idx <- which(plotSettings$names %in% unique(proxyDFprj$CategorySpecific))
+  refrenceRegShp <- subset(refregions, Acronym ==reg)
+  regMap <-  ggplot() +
+    geom_map(data=countries, map=fortify(countries),
+             aes(x=long, y=lat, group=group, map_id=id), 
+             fill = "grey80",color="grey90",size=0.4) +
+    coord_fixed(1,xlim= range(fortify(refrenceRegShp)$lon),
+                ylim=range(fortify(refrenceRegShp)$lat)) + 
+    geom_map(data=refrenceRegShp, map=fortify(refrenceRegShp), 
+             aes(x=long, y=lat, group=group, map_id=id),
+             fill=NA, alpha=0.75, size=0.5, color='black') +
+    geom_star(data=proxyDFprj,
+              aes(x=geo_longitude.1 , y=geo_latitude.1,
+                  starshape=CategorySpecific, fill=CategorySpecific),
+              size=4,color='Black',alpha=1,starstroke=0.5) + 
+    scale_fill_manual(values=plotSettings$colors[idx],name= 'Proxy Category') +
+    scale_starshape_manual(values=plotSettings$shapes[idx],name= 'Proxy Category') +
+    theme_void() + 
+    theme(legend.position = 'none') 
+  ggsave(file.path(githubDir,'Figures','Dashboard','_Summary',
+                   paste(count,'_',reg,'__regMap.png',sep='')),
+         plot=regMap,device='png',width=5,height=5,units='in')
+  ggsave(file.path(githubDir,'Figures','Dashboard','_Summary',
+                   paste(count,'_',reg,'__stackPlot.png',sep='')),
+         plot=stackPlot,device='png',width=8.5,height=11,units='in')
+  #
+  #
+  #
+}
+
+dir  <- getwd()# '/Volumes/GoogleDrive/My Drive/zResearch/Manuscript/HoloceneHydroclimate/HoloceneHydroclimate' #
+
+PROJ <- '+proj=robin   +ellps=WGS84 +datum=WGS84 +no_defs +lon_0=0 +x_0=0 +y_0=0 +units=m'
+PROJorig <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+load(url('https://github.com/SantanderMetGroup/ATLAS/blob/main/reference-regions/IPCC-WGI-reference-regions-v4_R.rda?raw=true'))
+refregions <-  spTransform(IPCC_WGI_reference_regions_v4, CRSobj = PROJ)
+
+
+
+for (j in c(1)){
+  for (ts in 1:length(TS_Poll)){
+    #df <- proxyDFprj %>% filter(paleoData_TSid==unique(proxyDFprj$paleoData_TSid)[i])
+    tso <- TS_Poll[[ts]]
+    col <- 'black' #plotSettings$colors[which(plotSettings$names == ts$CategorySpecific)]
+    shp <- 1#plotSettings$shapes[which(plotSettings$names == ts$CategorySpecific)]
+    df <- data.frame(
+      age    = as.numeric(tso$age), 
+      values = as.numeric(tso$paleoData_values)) %>%
+      filter(between(age,-100,12400)) %>%
+      arrange(age) # tso$paleoData_HoloceneValues
+    ages<-sort(c(df$age[1],df$age[-1]-diff(df$age)/2,df$age[-nrow(df)]+diff(df$age)/2,df$age[nrow(df)]))
+    values <- rep(df$values, each=2)
+    plt <- ggplot() +
+      geom_hline(yintercept=mean(df$values,na.rm=TRUE)) +
+      geom_star(aes(x=df$age,y=df$values),fill=col,color='black',starshape=shp,size=1,starstroke=0.5,alpha=0.7) + 
+      geom_path(aes(x=ages,y=values),color=col,alpha=0.7) + 
+      scale_x_reverse(name = "Age (yr BP)", limits=c(12000,0),expand=c(0,0),n.breaks=7,oob=scales::squish) +
+      theme_bw() +     
+      #ggtitle(paste(unique(df[,'geo_ipccRegion']),': ',unique(df[,'dataSetName']),sep='')) + 
+      theme(text = element_text(family='sans',size=8),
+            plot.title = element_text(hjust = 0.5,family='sans',size=10,face='bold'))
+    if (ts$climateInterpretation1_interpDirection == 'negative'){
+      plt <- plt + scale_y_reverse(name=paste(ts$paleoData_variableName,
+                                              ' (',ts$paleoData_units,')',sep=''))
+    } else{
+      plt <- plt + scale_y_continuous(name=paste(ts$paleoData_variableName,
+                                                 ' (',ts$paleoData_units,')',sep=''))
+    }
+    
+    map <- regMap + 
+      geom_star(data=df,aes(x=geo_longitude.1 , y=geo_latitude.1,
+                            starshape=CategorySpecific, fill=CategorySpecific),
+                size=4,color='gold',alpha=1,starstroke=4) +
+      theme(text = element_text(family='sans',size=8))
+    h <- 0
+    txt <- ggplot(df) 
+    for (name in c('paleoData_TSid','dataSetName',
+                   'geo_latitude','geo_longitude','geo_elevation',
+                   'archiveType','Category','CategorySpecific',
+                   'paleoData_proxyGeneral','paleoData_proxy','paleoData_proxyDetail','paleoData_variableName',
+                   'climateInterpretation1_seasonalityGeneral',
+                   'climateInterpretation1_variable','paleoData_units',
+                   'Source','pub1_title','pub1_doi','pub2_title','pub2_doi')){
+      h <- h-1
+      if (name %in% names(df) == FALSE){df[,name] <- NA}
+      txt <- txt + 
+        annotate("text", x = 0, y = h, label = paste(name,': ',sep=''),hjust = 0,size=2.5) +
+        annotate("text", x = 0.25, y = h, label = unique(df[,name]),hjust = 0,size=2.5)    
+    }
+    txt <- txt + scale_x_continuous(limits=c(0,0.6)) + theme_void() 
+    bkg <- ggplot()+
+      theme_void() +
+      theme(plot.background = element_rect(fill = NA,color='Black'))
+    summary <- ggdraw() + 
+      draw_plot(plt, x = 0, y = 0.5, width = 1, height = 0.5) +
+      draw_plot(map, x = 0, y = 0, width = 0.5, height = 0.5) +
+      draw_plot(txt, x = 0.5, y = 0, width = 0.5, height = 0.5) +
+      draw_plot(bkg, x = 0, y = 0, width = 1, height = 1) 
+    ggsave(file.path(githubDir,'Figures','Dashboard',
+                     paste(count,'_',
+                           unique(df[,'geo_ipccRegion']),'_',
+                           unique(df[,'geo_latitude']),'_',
+                           unique(df[,'dataSetName']),'_',
+                           unique(df[,'paleoData_TSid']),
+                           '.png',sep='')),
+           plot=summary,device='png',width=10,height=5,units='in')
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 #tablelist <- vector(mode='list')
 #for (archive in unique(proxyDf$archive)){
 #  summary <- proxyDf[which(proxyDf$archive == archive),]%>% group_by(proxy) %>% summarise(n = n())
